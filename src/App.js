9e66271ac2f2a7dfd962217ee35598fe0ad84225
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-07-19 10:07:55 +0700

    use context

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { createContext } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Menu from './components/Menu/Menu';
-import MainUseContext from './components/UseContext/MainUseContext';
-import MainUseReducer from './components/UseReducer/MainUseReducer';
-import TodoApp from './components/ToDoApp/TodoApp';
+
+// Context
+// 1. createContext
+// 2. Provider
+// 3. Consumer
+
+const themeContext = createContext()
 
 function App() {
   return (
-      <div className="container mt-2">
-        <div className='row'>
-          <div className='col-2'>
-            <Menu />
-          </div>
-          <div className='col-10'>
-            <Routes>
-              <Route path='/' element={<MainUseContext />} />
-              <Route path='/use-context' element={<MainUseContext />} />
-              <Route path='/use-reducer' element={<MainUseReducer />} />x
-              <Route path='/todo-app' element={<TodoApp />} />x
-            </Routes>
-          </div>
-        </div>
+      <div >
       </div>
   );
 }
---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Paragraph(){
+    return (
+        <div>
+            <p>Magna cillum ex fugiat aliquip eu anim voluptate enim ex. Tempor culpa sunt culpa incididunt nulla quis ea labore deserunt. Cillum nulla culpa veniam ut ipsum commodo do nulla commodo consectetur.</p>
+        </div>
+    )
+}
+
+export default Paragraph
