a3fa4444c99ef9a9e8a8f62ca58b89f579d509ba
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-07-19 10:27:21 +0700

    update template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-import { createContext } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
-// Context
-// 1. createContext
-// 2. Provider
-// 3. Consumer
-
-const themeContext = createContext()
 
 function App() {
   return (
